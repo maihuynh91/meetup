8e469c2cfa918b16811f8840b5a45a898d390005
Mai Huynh <nhumaihuynh91@gmail.com>
2018-12-24 21:58:52 -0600

    user seeds

---
+++ src/db/seeds/20181204230138-user.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const faker = require("faker");
+const bcrypt = require("bcryptjs");
+
+const salt = bcrypt.genSaltSync();
+let users = [];
+//pass needs to be at least 10 chars
+let password = "01233456789"
+const hashedPassword = bcrypt.hashSync(password, salt);
+
+for(let i = 1 ; i <= 20 ; i++){
+ users.push({
+   username: faker.internet.userName(),
+   email: faker.internet.email(),
+   password: hashedPassword,
+   role: "member",
+   createdAt: new Date(),
+   updatedAt: new Date(),
+ });
+}
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert("Users", users, {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("Users", null, {});
+  }
+
+
+};
